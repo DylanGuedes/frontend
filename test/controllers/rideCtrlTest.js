f2aebc38c26f4420ee1aa0030d7b233605a33192
João Pedro Sconetto <sconetto.joao@gmail.com>
2016-05-19 21:53:02 -0300

    Initial test implementation

--- test/controllers/rideCtrlTest.js
+++ test/controllers/rideCtrlTest.js
@@ -1,10 +1,18 @@
-describe('chatCtrl tests', function() {
-  beforeEach(module('app'));
+describe('rideCtrl tests', function() {
+  var scope, controller;
+  
+  beforeEach(module('starter.controllers'));
 
-  describe('#sendMessage', function() {
-    it('should do blabla', function() {
-      expect(true).toEqual(true)
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    controller = $controller('rideCtrl', { $scope: scope });
+   }));
+
+  describe('#expectStub to', function() {
+    it('return 5', function() {
+      var value = 1;
+      var testResult = scope.testStub(value);
+      expect(testResult).toEqual(5);
     });
   });
 });
-
