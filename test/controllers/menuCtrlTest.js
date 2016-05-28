ea69fd579979715c7ad43fd49b46af3edb72bc32
MiguelPimentel <miguelpimentel2013@gmail.com>
2016-05-27 23:21:01 -0300

    Adding test for function isLogged()

---
+++ test/controllers/menuCtrlTest.js
@@ -0,0 +1,52 @@
+describe('menuCtrl tests', function() {
+  describe('scope tests', function() {
+    var $rootScope, createController;
+    beforeEach(function() {
+      module('starter');
+      module('starter.controllers');
+      module('starter.services');
+    });
+
+    beforeEach(inject(function($injector) {
+      var $controller = $injector.get('$controller');
+      $rootScope = $injector.get('$rootScope');
+
+      createController = function() {
+        return $controller('menuCtrl', {'$scope': $rootScope});
+      };
+    }));
+
+  /*
+
+  it('should be defined and initializes', (function() {
+      var controller = createController();
+      expect(controller).toBeDefined();
+      expect($rootScope).toBeDefined();
+      expect($rootScope.remove).toBeDefined();
+      expect($rootScope.rides).toBeDefined();
+      expect($rootScope.rides).toEqual([]);
+      expect($rootScope.message).toEqual('');
+    }));
+  */
+
+    it('should be defined and initializes', (function() {
+      var controller = createController();
+      expect(controller).toBeDefined();
+      expect($rootScope).toBeDefined();       
+
+      }));
+
+    describe('User is logged', function(){
+
+      beforeEach(function() {
+        module('starter');
+        module('starter.controllers');
+        module('starter.services');
+      });
+
+      it('should be logged', function(){
+        expect(scope.isLogged()).toBeTruthy();
+      });
+    });
+  });
+});
